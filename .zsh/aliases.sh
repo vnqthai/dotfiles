203e70ec6e0cd1e8a888a864f2044d81264919d6
Thai <vnqthai@users.noreply.github.com>
2019-05-26 22:48:26 +0800

    Add alias ber

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -3,6 +3,7 @@ alias d="docker"
 alias dc="docker-compose"
 alias bi="bundle install"
 alias be="bundle exec"
+alias ber="bundle exec rails"
 alias berc="bundle exec rails console"
 alias bers="bundle exec rails server"
 
