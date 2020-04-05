b4f6f9266a3071dfd492c706fe88b933c8db9617
Thai <vnqthai@users.noreply.github.com>
2020-04-05 10:57:00 +0700

    Add python3.8 alias

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -1,9 +1,11 @@
 alias v="nvim"
+alias p="python3.8"
+
 alias d="docker"
 alias dc="docker-compose"
+
 alias bi="bundle install"
 alias be="bundle exec"
 alias ber="bundle exec rails"
 alias berc="bundle exec rails console"
 alias bers="bundle exec rails server"
-
